a051de2322df923004fd0820f2ba5bb28b34158e
jappleton2 <jim_appleton@gwinnett.k12.ga.us>
2015-03-20 10:03:54 -0400

    added graph

--- hs_data_pred_NSC/dual_enrollment.R
+++ hs_data_pred_NSC/dual_enrollment.R
@@ -54,3 +54,43 @@ SELECT distinct t1.[SCHOOL_YEAR]
 case.cols("dual.enr")
 table(dual.enr$school_year)
 table(dual.enr$title)
+
+# check on kids with both AP exams and dual enrollment
+    ap <- sqlQuery(ma_ch, paste0("
+
+            SELECT [STUNUMB]
+                  ,[TEST_KEY]
+                  ,[GRADE]
+                  ,[EXAM_ADMIN_DATE]
+                  ,[SCORE]
+                  ,[SCHOOL_YEAR]
+              FROM [Assessment].[dbo].[TEST_STU_APP]
+              WHERE SCHOOL_YEAR in ", paste("(", paste(yrs, collapse=","),")"), " and  
+                    SCORE is not null and 
+                    SCORE != 0
+        "))
+
+case.cols("ap")
+
+apStuMeanExam <- ddply(ap, "stunumb", summarise, ap_exam_mean = mean(score))
+dualStuMeanGrade <- ddply(dual.enr[!is.na(dual.enr$mark_numeric) & !is.na(dual.enr$credit_earned) 
+                                   & dual.enr$credit_earned > 0, ], "permnum", summarise, 
+                          dual_grade_mean = mean(mark_numeric))
+
+ap.dual <- merge(dualStuMeanGrade, apStuMeanExam, by.x = "permnum", by.y = "stunumb")
+
+p <- ggplot(data = ap.dual[complete.cases(ap.dual[, 2:3]), ], 
+            aes(dual_grade_mean, ap_exam_mean, group = 1))
+  p <- p + geom_point(size = 3, position = "Jitter", colour = "grey60")
+  p <- p + stat_smooth(span = 0.9, method = "loess", size = 1, colour = "black")
+  p <- p + stat_smooth(method = "lm", size = 1.5)
+  p <- p + scale_y_continuous()
+  p <- p + ggtitle(paste0("2012-14 Relationship Between Dual Enrollment Course Grade and AP Exam Score\nGCPS Correlation and Sample Size: r = ", round(cor(ap.dual[, 2], ap.dual[, 3], use = "pairwise.complete.obs"), 2), 
+                                    "; N = ", nrow(ap.dual)))
+p
+
+pdf("C:\\Users\\e200701890\\Desktop\\climate_ccrpi_2015.pdf", width = 12, height = 9)
+print(p)
+dev.off()
+
+
